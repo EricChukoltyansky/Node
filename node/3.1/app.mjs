e8ad1ce8915fe06c5ae7b662dcca330d5c0f36e3
EricChukoltyansky <kazohelmet@gmail.com>
2022-01-09 14:42:28 +0200

    import/export vs require

---
+++ node/3.1/app.mjs
@@ -0,0 +1,16 @@
+// const getNotes = require("./notes.js");
+
+import { function1 } from "./module.mjs";
+import { function2 } from "./module.mjs";
+import { function3 } from "./module.mjs";
+
+// const msg = getNotes();
+
+const func1 = function1();
+const func2 = function2();
+const func3 = function3();
+
+// console.log(msg);
+console.log(func1);
+console.log(func2);
+console.log(func3);
---
+++ node/3.1/module.mjs
@@ -0,0 +1,14 @@
+// import * as fs from 'fs'
+
+export function function1() {
+    return 'f1'
+    
+}
+export function function2() {
+    return 'f2'
+
+}
+export function function3() {
+    return 'f3'
+
+}
