e14a70b46aba952ad376744fa55e28b5f00a155b
EricChukoltyansky <kazohelmet@gmail.com>
2022-01-09 11:47:15 +0200

    first commit

---
+++ node/1.1.js
@@ -0,0 +1,26 @@
+// JS
+// 1. JS is a programming language that is used for writing script on the websites
+// 2. JS can only run in the browser
+// 3. It is basicly used on the client side
+// 4. JS is capable enough to add HTML and play with the DOM
+// 5. JS can run un any browserengine as like JS core in safari and Spidermonkey in Firefox
+// 6. JS is used in frontend dev
+// 7. Some of the JS frameworks are RamdaJS, TypedJs, etc
+// 8. It is the upgraded version of ECMA script that uses Chrome V8 engine written in C++
+
+// Node
+// 1. NodeJS is a JS runtime environment
+// 2. We can run JS outside the browser with the help of Nodejs
+// 3. It is mostly used on the server side
+// 4. Nodejs does not have the capability to add HTML tags
+// 5. V8 is the JS engine inside of node.js that parses and runs JS
+// 6. Nodejs is used in server-side dev
+// 7. Some of the Node js modules are Lodash,express etc. These modules are to be imported from npm
+// 8. Node.js is written in C,C++ and JS
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/EricChukoltyansky/Node.git
+// git push -u origin main
