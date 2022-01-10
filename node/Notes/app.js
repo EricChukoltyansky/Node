76a90eb900c091a6af77c09000536baaadb81894
EricChukoltyansky <kazohelmet@gmail.com>
2022-01-10 11:09:22 +0200

    body and title yargs

--- node/Notes/app.js
+++ node/Notes/app.js
@@ -1,19 +1,51 @@
 const chalk = require("chalk");
 const getNotes = require("./notes.js");
-const yargs = require('yargs')
+const yargs = require("yargs");
 
-// const msg = getNotes()
-// console.log(msg)
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+        describe: 'Note body',
+        demandOption:true,
+        type: 'string'
+    }
+  },
+  handler: function (argv) {
+    console.log("Title: " + argv.title);
+    console.log("Body: " + argv.body);
+  },
+});
 
-// const greenMsg = chalk.blue.inverse.bold('Success!')
-// console.log(greenMsg)
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  handler: function () {
+    console.log("Reading a note");
+  },
+});
 
-// console.log(process.argv[2])
+yargs.command({
+  command: "list",
+  describe: "List all notes",
+  handler: function () {
+    console.log("Listing all notes");
+  },
+});
 
-const command = process.argv[2];
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: function () {
+    console.log("Removing a note");
+  },
+});
 
-if (command === "add") {
-  console.log("Adding note");
-} else if (command === "remove") {
-  console.log("Removing note!");
-}
+// console.log(yargs.argv);
+yargs.parse()
