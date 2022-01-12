d663ca5be792360f1196aaad4c814823cb7b1b96
EricChukoltyansky <kazohelmet@gmail.com>
2022-01-12 14:48:40 +0200

    postman crud

--- node/10.1/postman.js
+++ node/10.1/postman.js
@@ -4,18 +4,43 @@ const app = express();
 const host = "localhost";
 const port = 8000;
 
+let data = [1, 2, 3, 4, 5, 6];
+
 app.get("/numbers", (req, res) => {
-  res.send("Success using GET");
+  res.status(200).json(data);
 });
-app.post("/numbers", (req, res) => {
-  res.send("Success using POST");
+
+app.post("/numbers/:newItem", (req, res) => {
+  const { newItem } = req.params;
+  req.body = +newItem;
+  data.push(req.body);
+  res.status(201).json(data);
 });
-app.put("/numbers", (req, res) => {
-  res.send("Success using PUT");
+
+app.delete("/numbers/:num", (req, res) => {
+  const { num } = req.params;
+  let found = data.find((item) => item === +num);
+  console.log(found);
+  if (found) {
+    let targetIndex = data.indexOf(found);
+    data.splice(targetIndex, 1);
+  }
+  console.log(data);
+  res.json(data);
 });
-app.delete("/numbers", (req, res) => {
-  res.send("Success using DELETE");
+
+app.put("/numbers/:num", (req, res) => {
+  const { num } = req.params;
+  const { newNum } = req.query;
+  const itemIndex = data.findIndex((item) => item === +num);
+  data[itemIndex] = +newNum;
+  res.json(data);
 });
+app.get("*", (req, res) =>
+  res
+    .status(404)
+    .json({ message: "Route does not exist", app: "Express-Routes" })
+);
 
 app.listen(port, host, () => {
   console.log(`LIstening on port: http://${host}:${port}`);
