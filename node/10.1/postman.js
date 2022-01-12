8d047dc9e909224a8351104ee27fcb73f5507f7a
EricChukoltyansky <kazohelmet@gmail.com>
2022-01-12 20:40:03 +0200

    handlebar

--- node/10.1/postman.js
+++ node/10.1/postman.js
@@ -1,20 +1,47 @@
 const express = require("express");
 const app = express();
+const hbs = require("hbs");
 
 const host = "localhost";
 const port = 8000;
 
 let data = [1, 2, 3, 4, 5, 6];
 
+let demo = {
+  name: "Rohan",
+  age: 26,
+};
+
+let projects = {
+  name: "Rahul",
+  skills: ["Data Mining", "BlockChain Dev", "node.js"],
+};
+
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render("dynamic", { demo: demo });
+});
+
+// app.get("/projects", (req, res) => {
+//   res.render("projects", { project: projects });
+// });
+
 app.get("/numbers", (req, res) => {
-  res.status(200).json(data);
+  res.sendStatus(200).json(data);
 });
 
 app.post("/numbers/:newItem", (req, res) => {
   const { newItem } = req.params;
-  req.body = +newItem;
-  data.push(req.body);
-  res.status(201).json(data);
+  let found = data.find((item) => item === +newItem);
+  console.log(found);
+  if (found) {
+    res.sendStatus(400).send("Number already exists");
+  } else {
+    req.body = +newItem;
+    data.push(req.body);
+    res.sendStatus(201).json(data);
+  }
 });
 
 app.delete("/numbers/:num", (req, res) => {
@@ -24,21 +51,28 @@ app.delete("/numbers/:num", (req, res) => {
   if (found) {
     let targetIndex = data.indexOf(found);
     data.splice(targetIndex, 1);
+    console.log(data);
+    res.json(data);
+  } else {
+    res.sendStatus(400).send("No such number");
   }
-  console.log(data);
-  res.json(data);
 });
 
 app.put("/numbers/:num", (req, res) => {
   const { num } = req.params;
   const { newNum } = req.query;
   const itemIndex = data.findIndex((item) => item === +num);
-  data[itemIndex] = +newNum;
-  res.json(data);
+  if (itemIndex === -1) {
+    res.sendStatus(400).send("No such number");
+  } else {
+    data[itemIndex] = +newNum;
+    res.json(data);
+  }
 });
+
 app.get("*", (req, res) =>
   res
-    .status(404)
+    .sendStatus(404)
     .json({ message: "Route does not exist", app: "Express-Routes" })
 );
 
