ba809481d09487643ef5e9e3bc0fb4d1e8a3db69
EricChukoltyansky <kazohelmet@gmail.com>
2022-01-17 14:15:13 +0200

    all restaurants

---
+++ node/15.1/restaurant.js
@@ -0,0 +1,140 @@
+[
+  {
+    _id: {
+      $oid: "61e5577e52659ac2e1b5350f",
+    },
+    name: "Hubert",
+    address: {
+      city: "Sydney",
+      street: "Bligh 15",
+      coordinates: "-77,564, 40.677",
+    },
+    type: "French",
+    kohser: "no",
+    reviews: [
+      {
+        date: "22-10-2022",
+        score: "10",
+      },
+      {
+        date: "22-10-2022",
+        score: "9",
+      },
+    ],
+  },
+  {
+    _id: {
+      $oid: "61e557ce52659ac2e1b53510",
+    },
+    name: "Mr Wong",
+    address: {
+      city: "Sydney",
+      street: "3 Bridge Ln ",
+      coordinates: "-77,564, 40.677",
+    },
+    type: "Chinese",
+    kohser: "no",
+    reviews: [
+      {
+        date: "22-10-2022",
+        score: "10",
+      },
+      {
+        date: "22-10-2022",
+        score: "9",
+      },
+    ],
+  },
+  {
+    _id: {
+      $oid: "61e557f852659ac2e1b53511",
+    },
+    name: "Bennelong",
+    address: {
+      city: "Sydney",
+      street: "Opera House ",
+      coordinates: "-77,564, 40.677",
+    },
+    type: "Modern Australian",
+    kohser: "no",
+    reviews: [
+      {
+        date: "22-10-2022",
+        score: "10",
+      },
+      {
+        date: "22-10-2022",
+        score: "9",
+      },
+    ],
+  },
+  {
+    _id: {
+      $oid: "61e5584652659ac2e1b53512",
+    },
+    name: "Spice Temple",
+    address: {
+      city: "Sydney",
+      street: "Bligh st.",
+      coordinates: "-77,564, 40.677",
+    },
+    type: "Chinese",
+    kohser: "no",
+    reviews: [
+      {
+        date: "22-10-2022",
+        score: "10",
+      },
+      {
+        date: "22-10-2022",
+        score: "9",
+      },
+    ],
+  },
+  {
+    _id: {
+      $oid: "61e55c0d22b8388965574a79",
+    },
+    name: "Vue de Mond",
+    address: {
+      city: "Melbourne",
+      street: "Collins st.",
+      coordinates: "-77,564, 40.677",
+    },
+    type: "Contemporary",
+    kohser: "no",
+    reviews: [
+      {
+        date: "22-10-2022",
+        score: "10",
+      },
+      {
+        date: "22-10-2022",
+        score: "9",
+      },
+    ],
+  },
+  {
+    _id: {
+      $oid: "61e55c6122b8388965574a7a",
+    },
+    name: "WildFlower",
+    address: {
+      city: "Perth",
+      street: "Cathedral Av.",
+      coordinates: "-77,564, 40.677",
+    },
+    type: "Contemporary",
+    kohser: "no",
+    reviews: [
+      {
+        date: "22-10-2022",
+        score: "10",
+      },
+      {
+        date: "22-10-2022",
+        score: "9",
+      },
+    ],
+  },
+];
