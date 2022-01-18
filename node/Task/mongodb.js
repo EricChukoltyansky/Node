fb1756a94547cb63afd1a4a78a5dad562dc7e2d4
EricChukoltyansky <kazohelmet@gmail.com>
2022-01-18 13:47:34 +0200

    mongoose model

--- node/Task/mongodb.js
+++ node/Task/mongodb.js
@@ -1,43 +1,30 @@
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+// CRUD create read update delete
 
-const connectionURL = "mongodb://localhost:27017";
-const databaseName = "task-manager";
+const { MongoClient, ObjectID } = require('mongodb')
 
-MongoClient.connect(
-  connectionURL,
-  { useNewUrlParser: true },
-  (error, client) => {
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
-      return console.log("Unable to connect to database");
+        return console.log('Unable to connect to database!')
     }
-    console.log("connected");
-    const db = client.db(databaseName);
 
-    // db.collection("users").insertOne({
-    //   name: "Eric",
-    //   age: 32,
-    // },(error, result)=>{
-    //     if(error) {
-    //         return console.log('Unable to insert user')
-    //     }
+    const db = client.db(databaseName)
+    
+    // db.collection('users').deleteMany({
+    //     age: 27
+    // }).then((result) => {
     //     console.log(result)
-    // });
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
 
-    db.collection("users").insertMany([
-      {
-        name: "Jen",
-        age: 27,
-      },
-      {
-        name: "Gunthar",
-        age: 31,
-      },
-    ], (error, result) => {
-        if (error) {
-            return console.log('Unable to insert docs')
-        }
+    db.collection('tasks').deleteOne({
+        description: "Clean the house"
+    }).then((result) => {
         console.log(result)
-    });
-  }
-);
+    }).catch((error) => {
+        console.log(error)
+    })
+})
